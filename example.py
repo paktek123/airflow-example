74a00ceb518c8dfb78e3cb0e8eda44a67236b6f6
Neeran Gul <paktek123@gmail.com>
2021-04-17 17:29:48 +0100

    Update example.py

--- example.py
+++ example.py
@@ -22,9 +22,9 @@ dag = DAG(
 start = DummyOperator(task_id='run_this_first', dag=dag)
 
 passing = KubernetesPodOperator(namespace='default',
-                          image="Python:3.6",
-                          cmds=["Python","-c"],
-                          arguments=["print('hello world')"],
+                          image="bde2020/spark-submit",
+                          cmds=["sleep"],
+                          arguments=["10000000"],
                           labels={"foo": "bar"},
                           name="passing-test",
                           task_id="passing-task",
@@ -32,16 +32,16 @@ passing = KubernetesPodOperator(namespace='default',
                           dag=dag
                           )
 
-failing = KubernetesPodOperator(namespace='default',
-                          image="ubuntu:1604",
-                          cmds=["Python","-c"],
-                          arguments=["print('hello world')"],
-                          labels={"foo": "bar"},
-                          name="fail",
-                          task_id="failing-task",
-                          get_logs=True,
-                          dag=dag
-                          )
+#failing = KubernetesPodOperator(namespace='default',
+#                          image="ubuntu:1604",
+#                          cmds=["Python","-c"],
+#                          arguments=["print('hello world')"],
+#                          labels={"foo": "bar"},
+#                          name="fail",
+#                          task_id="failing-task",
+#                          get_logs=True,
+#                          dag=dag
+#                          )
 
 passing.set_upstream(start)
-failing.set_upstream(start)
+#failing.set_upstream(start)
